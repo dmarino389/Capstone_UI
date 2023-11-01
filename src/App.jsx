62efb056b0e4ef6d7b0a8b2b80b5b5cb554a1ad0
dmarino389 <139820583+dmarino389@users.noreply.github.com>
2023-11-01 13:23:39 -0500

    Work in progress, giving me an error

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { component } from 'react'
+
+export default class App extends Component {
+
+  
+  render() {
+    console.log('I am rendering')
+    return (
+      <div> 
+      
+       
+      </div>
+    )
+  }
+}
+
+
+
+
+
+
+
+
+
